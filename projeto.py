bde66658619f14b27fece314abd1729d1cac9ed6
marianamduarte1 <marianamduarte1@hotmail.com>
2025-10-10 11:52:06 +0100

    um pouco do PCA

--- projeto.py
+++ projeto.py
@@ -20,10 +20,9 @@ Classes=["Healthy","Cancer"]
 dados['Classification'].iloc[ixHealthy]=Classes[0]
 dados['Classification'].iloc[ixCancer]=Classes[1]
 
-fnames=dados.columns[0:-1]#Get the feature names
+fnames=dados.columns[0:-1]
 
-
-"---------Kruskal-Wallis H-test---------"
+#---------Kruskal-Wallis H-test---------
 
 H_rank=[]
 
@@ -39,17 +38,14 @@ Hs=sorted(H_rank, key=lambda x: x[1], reverse=True)
 for f in Hs:
     print(f"Feature: {f[0]}, H-statistic: {f[1]:.4f}")
 
-"---------ROC, AUC---------"
+#---------ROC, AUC---------
 
 ixHealthyCancer=np.concatenate((ixHealthy[0],ixCancer[0]))
 y=dados['Classification'].to_numpy()[ixHealthyCancer]
 roc_auc=np.zeros(fnames.shape)
 i=0
 
-
-
 for f in fnames:#Go along features
-
 
     fpr, tpr, _= roc_curve(y,dados[f].to_numpy()[ixHealthyCancer],pos_label="Cancer")
 
@@ -74,15 +70,40 @@ print("Sorting accourding to ROC-AUC:")
 for i in sortIx:
     print(fnames[i]+"-->"+str(roc_auc[i]))
 
-"Se AUC for a feature is 0.5, it means that the feature does not discriminate between the two classes"
-"ou seja, a feature aumenta com Cancer"
+#Se AUC for a feature is 0.5, it means that the feature does not discriminate between the two classes
+# ou seja, a feature aumenta com Cancer
 
-"------CORRELATION MATRIX-------"
+#------CORRELATION MATRIX-------
 X=np.array([dados[Hs[0][0]],dados[Hs[1][0]],dados[Hs[2][0]],dados[Hs[3][0]],dados[Hs[4][0]],dados[Hs[5][0]],dados[Hs[6][0]],dados[Hs[7][0]],dados[Hs[8][0]]])
 
 corrMat=np.corrcoef(X)
-fig= px.imshow(corrMat, x=[Hs[0][0],Hs[1][0],Hs[2][0],Hs[3][0],Hs[4][0],Hs[5][0],Hs[6][0],Hs[7][0],Hs[8][0]], y=[Hs[0][0],Hs[1][0],Hs[2][0],Hs[3][0],Hs[4][0],Hs[5][0],Hs[6][0],Hs[7][0],Hs[8][0]], color_continuous_scale='RdBu', title='Correlation Matrix of Top 10 Features')
+fig= px.imshow(corrMat, x=[Hs[0][0],Hs[1][0],Hs[2][0],Hs[3][0],Hs[4][0],Hs[5][0],Hs[6][0],Hs[7][0],Hs[8][0]], y=[Hs[0][0],Hs[1][0],Hs[2][0],Hs[3][0],Hs[4][0],Hs[5][0],Hs[6][0],Hs[7][0],Hs[8][0]], color_continuous_scale='RdBu', title='Correlation Matrix of Top 9 Features')
 fig.show()
+
+#PCA
+
+#---------PCA---------
+
+X=dados.to_numpy()[:,:-1] #todas as colunas menos a última 
+y=dados.to_numpy()[:,9] #só a coluna da Classificação
+X=X.astype(float)
+#Normalize
+X=(X-np.mean(X,axis=0))/np.std(X,axis=0)
+
+from sklearn.decomposition import PCA
+pca = PCA()
+pca.fit(X)
+#PCA eigenvalues/Explained variance
+print("PCA eigenvalues/Explained variance")
+print(pca.explained_variance_)
+print("Sum of eigenvalues="+str(np.sum(pca.explained_variance_)))
+#PCA eigenvectors/Principal components
+print("PCA eigenvectors/Principal components")
+W=pca.components_.T
+print(W)
+
+
+
 
 
 
